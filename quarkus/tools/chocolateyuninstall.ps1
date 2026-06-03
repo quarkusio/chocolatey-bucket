26e8e1b98b7ba94425e4f15780740de95f990cb9
jreleaserbot <jreleaser@kordamp.org>
2026-06-03 11:48:48 +0000

    quarkus 3.36.1

--- quarkus/tools/chocolateyuninstall.ps1
+++ quarkus/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.3.0 at 2026-05-27T07:31:56.266504229Z
+# Generated with JReleaser 1.3.0 at 2026-06-03T11:48:35.969893204Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'quarkus-cli-3.36.0'
+$app_home = Join-Path $package 'quarkus-cli-3.36.1'
 $app_exe = Join-Path $app_home 'bin/quarkus.bat'
 
 Uninstall-BinFile -Name 'quarkus' -Path $app_exe
