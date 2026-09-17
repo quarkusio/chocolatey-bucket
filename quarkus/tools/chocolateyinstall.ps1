ca7ca48fd789098be6814049dd104b4a8eaf99d9
jreleaserbot <jreleaser@kordamp.org>
2026-09-17 09:47:58 +0000

    quarkus 3.39.4

--- quarkus/tools/chocolateyinstall.ps1
+++ quarkus/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-# Generated with JReleaser 1.3.0 at 2026-09-10T12:13:37.032266131Z
+# Generated with JReleaser 1.3.0 at 2026-09-17T09:47:44.405015564Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'quarkus-cli-3.39.3'
+$app_home = Join-Path $package 'quarkus-cli-3.39.4'
 $app_exe = Join-Path $app_home 'bin/quarkus.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'quarkus' `
-    -Url 'https://github.com/quarkusio/quarkus/releases/download/3.39.3/quarkus-cli-3.39.3.zip' `
-    -Checksum '37710c298be2cfd287296cfa29f3e71648a71097f37dd673bb823eeb538a0aa4' `
+    -Url 'https://github.com/quarkusio/quarkus/releases/download/3.39.4/quarkus-cli-3.39.4.zip' `
+    -Checksum 'b5c8f8f08aa003d0f677a909af41143c9befb66ea089d8337324e93420c939bd' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
--- quarkus/tools/chocolateyuninstall.ps1
+++ quarkus/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.3.0 at 2026-09-10T12:13:37.032266131Z
+# Generated with JReleaser 1.3.0 at 2026-09-17T09:47:44.405015564Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'quarkus-cli-3.39.3'
+$app_home = Join-Path $package 'quarkus-cli-3.39.4'
 $app_exe = Join-Path $app_home 'bin/quarkus.bat'
 
 Uninstall-BinFile -Name 'quarkus' -Path $app_exe
